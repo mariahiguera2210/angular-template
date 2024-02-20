38f09c2e73b6651138cf1d5a167c0fdb9eadc383
mariahiguera2210 <angelica.higueram92@gmail.com>
2024-02-20 11:21:36 -0500

    se hace mostrar template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-template';
+  visible = true
+  
+  mostrar() {
+    this.visible = !this.visible;
+}
+
 }
